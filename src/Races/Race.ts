db05215b7c7dca6d2795cd084ebebb24f4172142
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-09-22 15:03:21 -0300

    refactor: Rafatorei a Classe Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,11 +1,5 @@
 export default abstract class Race {
-  protected counter = 0;
-
-  constructor(private _name: string, private _dexterity: number) {
-    this._name = _name;
-    this._dexterity = _dexterity;
-    this.counter = Race.createdRacesInstances();
-  }
+  constructor(private _name: string, private _dexterity: number) {}
 
   get name(): string {
     return this._name;
