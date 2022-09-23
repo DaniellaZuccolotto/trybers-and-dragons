1b1e0b5ab19a0d3fbecc330f1dcb318ea2477279
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-09-23 14:58:44 -0300

    fix arrumei a funcao runBattles

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ export const player1 = new Character('Player1');
 export const player2 = new Character('Player2');
 export const player3 = new Character('Player3');
 
-for (let i = 0; i < 50; i += 1) player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
 
 export const monster1 = new Monster();
 export const monster2 = new Dragon();
@@ -16,8 +19,8 @@ export const pvp = new PVP(player2, player3);
 
 export const pve = new PVE(player1, [monster1, monster2]);
 
-export function runBlattles(battles: Battle[]) {
-  battles.forEach((battle: Battle) => {
+export function runBattles(battles: Battle[]) {
+  battles.forEach((battle) => {
     battle.fight();
   });
 }
